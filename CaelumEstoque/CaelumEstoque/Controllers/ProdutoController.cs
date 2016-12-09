077bc9638b7f9c8a2bddaae7ce49da613da713a4
cskies <cskies@hotmail.com>
2016-12-09 11:00:56 -0200

    aula 4 da alura e testando comandos git

--- CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
+++ CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using CaelumEstoque.DAO;
 using CaelumEstoque.Models;
+using System.Collections;
 
 namespace CaelumEstoque.Controllers
 {
@@ -20,6 +21,29 @@ namespace CaelumEstoque.Controllers
             ViewBag.Produtos = produtos;
 
             return View();
+        }
+
+        public ActionResult Form()
+        {
+            //manda pra view a lista de categorias
+            var categoriasDao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = categoriasDao.Lista();
+            ViewBag.Categorias = categorias;
+
+            return View();
+        }
+
+        //os campos a receber da view form, sao passados como args
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            //gravando no bd
+            var dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+
+            //modelbinder cria o obj para aspnetmvc
+
+            return RedirectToAction("Index"); //se for contr diferente, eh soh passar como arg
         }
     }
 }
